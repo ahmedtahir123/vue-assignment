60b62cc1a2ca3e854c342af2a9c186ef68ff6756
Muhammad Ahmed <amuhammad@NIS-PK429.local>
2021-09-16 17:20:18 +0500

    add routing

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,23 @@
   <v-app>
    <Header/>
     <v-main>
-     <HelloWorld/>
+     <router-view/>
     </v-main>
-    
+    <Footer/>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
     Header,
+    Footer
   },
 
   data: () => {
